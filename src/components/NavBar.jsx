53914bcecb9d54a1f38edf6cd2a144ba76e420e1
Atroopa <atroopa@gmail.com>
2022-12-08 18:51:38 +0330

    make links array

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
+
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 5,
+            link: 'contact'
+        }
+    ]
+
   return (
-    <div className='bg-black flex justify-between items-center w-full h-20 text-white fixed'>
+    <div className='bg-black flex justify-between items-center w-full h-20 text-white fixed px-4'>
         <div>
-            <h1>Omid</h1>
+            <h1 className='text-5xl font-signature select-none ml-2'>Omid</h1>
         </div>
+
+        <ul className='flex'>
+            <li className='px-4 cursor-pointer capitalize font-medium
+                         text-gray-500 hover:scale-105 duration-200'>home</li>
+        </ul>
+
     </div>
   )
 }
