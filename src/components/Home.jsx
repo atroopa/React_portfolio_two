f57f91e46cc00f5061671ebc2e796f634dd85f80
Atroopa <atroopa@gmail.com>
2022-12-09 19:38:05 +0330

    home page work true

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,38 @@ import {MdOutlineKeyboardArrowRight} from  'react-icons/md';
 
 const Home = () => {
   return (
-    <div>
-        
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full ">
+              <h2 className='text-4xl sm:text-7xl font-bold text-white'>
+                i'm a Blockchain 
+                Developer
+              </h2>
+
+              <p className="text-gray-500 py-4 max-w-md">
+                i have 2 years of experience building and designing
+                software.
+                Currently, ilove to work on web application using
+                technologies like
+                React, Tailwind, solidity, Web3.js
+              </p>
+
+              <div>
+                <button>
+                  Portfolio 
+                  <span>
+                    <MdOutlineKeyboardArrowRight/>
+                  </span>
+                </button>
+              </div>
+        </div>
+            <div>
+                    <img src={HeroImage} alt='My Profile' className='rounded-2xl mx-auto w-2/3 md:w-full ' />
+                  </div>
+            </div>
     </div>
   );
 }
